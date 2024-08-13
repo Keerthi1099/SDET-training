a8c2e17870727482987a4e7bd99ea450ea4fa5f5
Keerthi1099 <yendreddykeerthi@gmail.com>
2024-08-12 21:02:17 -0500

    chore: indentation

--- src/com/training/java/TestVariables.java
+++ src/com/training/java/TestVariables.java
@@ -4,17 +4,16 @@ public class TestVariables {
 
 	public static void main(String[] args) {
 		
-		 System.out.println("Hello world!");
-		@SuppressWarnings("unused")
+	 	System.out.println("Hello world!");
 		short abc =12;
-        byte ab =122;
-        int a=123234;
-        long b = 1234567876543L;
-        float c = 123.23f;
-        double d =1231.23234;
-        String e ="Hello ";
-        System.out.println("Welcome");
-        System.out.println(abc);
+	        byte ab =122;
+	        int a=123234;
+	        long b = 1234567876543L;
+	        float c = 123.23f;
+	        double d =1231.23234;
+	        String e ="Hello ";
+	        System.out.println("Welcome");
+	        System.out.println(abc);
         
 	}
 
